ff0726d8d8b940a5c2c0285eb3c23f19ce9d0903
Manoel Martins <manoel.martins@muralis.com.br>
2023-08-12 21:33:16 -0300

    :sparkles: add test for video service

---
+++ application/services/video_service_test.go
@@ -0,0 +1,52 @@
+package services_test
+
+import (
+	"log"
+	"testing"
+	"time"
+
+	"github.com/ManoMartins/video-encoder/application/repositories"
+	"github.com/ManoMartins/video-encoder/application/services"
+	"github.com/ManoMartins/video-encoder/domain"
+	"github.com/ManoMartins/video-encoder/framework/database"
+	"github.com/joho/godotenv"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func prepare() (*domain.Video, repositories.VideoRepositoryDb) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	return video, repo
+}
+
+func TestVideoServiceDownload(t *testing.T) {
+	video, repo := prepare()
+	videoService := services.NewVideoService()
+
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("path")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+}
